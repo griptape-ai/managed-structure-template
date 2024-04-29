3b2e8c27c15ba8aa15a6f81af48e0b0073a2dfb0
Collin Dutter <collin@griptape.ai>
2024-04-29 14:18:16 -0700

    Feature/logs (#6)
    * Update to use logs
    
    * Rename set
    
    * Other minor formatting fixes

--- example-client/utils.py
+++ example-client/utils.py
@@ -1,15 +1,19 @@
+import os
 import requests
+
+
+GT_CLOUD_BASE_URL = os.environ.get("GT_CLOUD_BASE_URL", "http://127.0.0.1:5000")
 
 
 def print_streaming_events(
     events: list[dict],
-    printed_events: set,
+    printed_event_ids: set,
 ) -> set:
     """Print all `CompletionChunkEvent` tokens.
 
     Args:
         events: The events to print.
-        printed_events: The set of ids of already printed events. Used to prevent duplicate printing.
+        printed_event_ids: The set of ids of already printed events. Used to prevent duplicate printing.
     Returns:
         The updated set of printed events.
     """
@@ -17,14 +21,14 @@ def print_streaming_events(
         event
         for event in events
         if event["value"]["type"] == "CompletionChunkEvent"
-        and event["event_id"] not in printed_events
+        and event["event_id"] not in printed_event_ids
     ]
 
     for completion_event in new_completion_events:
         print(completion_event["value"]["token"], flush=True, end="")
-        printed_events.add(completion_event["event_id"])
+        printed_event_ids.add(completion_event["event_id"])
 
-    return printed_events
+    return printed_event_ids
 
 
 def create_structure_run(structure_id: str, env: dict, args: list) -> dict:
@@ -39,7 +43,8 @@ def create_structure_run(structure_id: str, env: dict, args: list) -> dict:
         The created Structure Run.
     """
     response = requests.post(
-        f"{HOST}/api/structures/{structure_id}/runs", json={"env": env, "args": args}
+        f"{GT_CLOUD_BASE_URL}/api/structures/{structure_id}/runs",
+        json={"env": env, "args": args},
     )
     response.raise_for_status()
 
@@ -55,7 +60,7 @@ def get_structure_run(run_id: str) -> dict:
     Returns:
         The Structure Run.
     """
-    response = requests.get(f"{HOST}/api/structure-runs/{run_id}")
+    response = requests.get(f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}")
     response.raise_for_status()
 
     return response.json()
@@ -71,7 +76,24 @@ def get_structure_run_events(run_id: str) -> dict:
         The events for the Structure Run.
     """
     response = requests.get(
-        f"{HOST}/api/structure-runs/{run_id}/events",
+        f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}/events",
+    )
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_structure_run_logs(run_id: str) -> dict:
+    """Get all logs for a run.
+
+    Args:
+        run_id: The Structure Run ID.
+
+    Returns:
+        The logs for the Structure Run.
+    """
+    response = requests.get(
+        f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}/logs",
     )
     response.raise_for_status()
 
--- structure.py
+++ structure.py
@@ -26,27 +26,28 @@ def is_running_in_managed_environment() -> bool:
 
 def get_listener_api_key() -> str:
     """The event driver expects a Griptape API Key as a parameter.
-    When your program is running in Griptape Cloud, you will need to provide a 
+    When your program is running in Griptape Cloud, you will need to provide a
     valid Griptape API Key in the GT_CLOUD_API_KEY environment variable, otherwise
-    the service will not authorize the necessary calls. 
-    You can create an API Key by visiting https://cloud.griptape.ai/keys 
+    the service will not authorize the necessary calls.
+    You can create an API Key by visiting https://cloud.griptape.ai/keys
     When running in Skatepark, the API key is not needed since it isn't validating calls.
 
     Returns:
         The Griptape API Key to authorize calls.
     """
-    api_key = os.environ.get("GT_CLOUD_API_KEY")
+    api_key = os.environ.get("GT_CLOUD_API_KEY", "")
     if is_running_in_managed_environment() and not api_key:
-        print("""
+        print(
+            """
               ****WARNING****: No value was found for the 'GT_CLOUD_API_KEY' environment variable.
               This environment variable is required when running in Griptape Cloud for authorization.
               You can generate a Griptape API Key by visiting https://cloud.griptape.ai/keys.
               Specify it as an environment variable when creating a Managed Structure in Griptape Cloud.
               """
-              )
+        )
     return api_key
 
-  
+
 # If running completely local, such as within an IDE, load environment vars.
 if not is_running_in_managed_environment():
     load_dotenv()
@@ -56,15 +57,13 @@ input = sys.argv[1]
 # We need an event driver to communicate events from our program back to our
 # host, which could be the locally-run Skatepark or Griptape Cloud.
 # The event driver requires a URL to the host.
-# When running in Skatepark or Griptape Cloud, this value is automatically provided to 
-# you in the environment variable GT_CLOUD_BASE_URL. You can override 
+# When running in Skatepark or Griptape Cloud, this value is automatically provided to
+# you in the environment variable GT_CLOUD_BASE_URL. You can override
 # this value by specifying your own for GriptapeCloudEventListenerDriver.base_url.
-event_driver = GriptapeCloudEventListenerDriver(
-    api_key=get_listener_api_key()
-)
+event_driver = GriptapeCloudEventListenerDriver(api_key=get_listener_api_key())
 
 #### BEGIN EXAMPLE: USING A GRIPTAPE AGENT
-# This example uses a Griptape Agent. The Agent will generate the correct events to indicate 
+# This example uses a Griptape Agent. The Agent will generate the correct events to indicate
 # if the run has succeeded or failed.
 structure = Agent(
     tools=[Calculator(off_prompt=False)],
@@ -75,7 +74,7 @@ structure.run(input)
 #### END EXAMPLE
 
 #### BEGIN EXAMPLE: NOT USING A GRIPTAPE STRUCTURE
-# If you wish to run a program that is NOT a Griptape Structure (e.g., Agent, Pipeline, Workflow, etc.), 
+# If you wish to run a program that is NOT a Griptape Structure (e.g., Agent, Pipeline, Workflow, etc.),
 # we will have to manually publish the FinishStructureRunEvent.
 # To use this example:
 # 1. Comment out the previous example
@@ -91,3 +90,4 @@ structure.run(input)
 
 # event_driver.publish_event(done_event)
 #### END EXAMPLE
+
