dd859d6c6c1b4aac39eb794b4db5495ce296d072
James Clarendon <james@griptape.ai>
2024-04-29 15:14:28 -0700

    Updated Utils to take API key, host for local & cloud testing

--- example-client/utils.py
+++ example-client/utils.py
@@ -1,8 +1,4 @@
-import os
 import requests
-
-
-GT_CLOUD_BASE_URL = os.environ.get("GT_CLOUD_BASE_URL", "http://127.0.0.1:5000")
 
 
 def print_streaming_events(
@@ -31,10 +27,28 @@ def print_streaming_events(
     return printed_event_ids
 
 
-def create_structure_run(structure_id: str, env: dict, args: list) -> dict:
+def generate_headers(api_key: str) -> dict:
+    """Generates headers for Griptape Cloud API calls
+
+    Args:
+        api_key: a Griptape Cloud API Key. This is ignored when running the Skatepark emulator, but required for a Griptape Cloud hosted Structure.
+    
+    Returns:
+        Dictionary for the header field in a requests call.
+    """
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+    return headers
+
+
+def create_structure_run(host: str, api_key: str, structure_id: str, env: dict, args: list) -> dict:
     """Create a Structure Run.
 
     Args:
+        host: the host URL for the Structure.
+        api_key: a Griptape Cloud API Key. This is ignored when running the Skatepark emulator, but required for a Griptape Cloud hosted Structure.
         structure_id: The Structure ID.
         env: The environment variables to pass to the Structure program.
         args: The arguments to pass to the Structure program.
@@ -43,57 +57,62 @@ def create_structure_run(structure_id: str, env: dict, args: list) -> dict:
         The created Structure Run.
     """
     response = requests.post(
-        f"{GT_CLOUD_BASE_URL}/api/structures/{structure_id}/runs",
-        json={"env": env, "args": args},
+        f"{host}/api/structures/{structure_id}/runs", json={"env": env, "args": args}, headers=generate_headers(api_key)
     )
     response.raise_for_status()
 
     return response.json()
 
 
-def get_structure_run(run_id: str) -> dict:
+def get_structure_run(host: str, api_key: str, run_id: str) -> dict:
     """Get a Structure Run status.
 
     Args:
+        host: the host URL for the Structure.
+        api_key: a Griptape Cloud API Key. This is ignored when running the Skatepark emulator, but required for a Griptape Cloud hosted Structure.
         run_id: The Structure Run ID.
 
     Returns:
         The Structure Run.
     """
-    response = requests.get(f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}")
+    response = requests.get(f"{host}/api/structure-runs/{run_id}", headers=generate_headers(api_key))
     response.raise_for_status()
 
     return response.json()
 
 
-def get_structure_run_events(run_id: str) -> dict:
+def get_structure_run_events(host: str, api_key: str, run_id: str) -> dict:
     """Get all events for a run.
 
     Args:
+        host: the host URL for the Structure.
+        api_key: a Griptape Cloud API Key. This is ignored when running the Skatepark emulator, but required for a Griptape Cloud hosted Structure.
         run_id: The Structure Run ID.
 
     Returns:
         The events for the Structure Run.
     """
     response = requests.get(
-        f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}/events",
+        f"{host}/api/structure-runs/{run_id}/events",
     )
     response.raise_for_status()
 
     return response.json()
 
 
-def get_structure_run_logs(run_id: str) -> dict:
+def get_structure_run_logs(host: str, api_key: str, run_id: str) -> dict:
     """Get all logs for a run.
 
     Args:
+        host: the host URL for the Structure.
+        api_key: a Griptape Cloud API Key. This is ignored when running the Skatepark emulator, but required for a Griptape Cloud hosted Structure.
         run_id: The Structure Run ID.
 
     Returns:
         The logs for the Structure Run.
     """
     response = requests.get(
-        f"{GT_CLOUD_BASE_URL}/api/structure-runs/{run_id}/logs",
+        f"{host}/api/structure-runs/{run_id}/logs",
     )
     response.raise_for_status()
 
