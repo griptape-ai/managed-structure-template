53773770778996af0985345ba5561dd2daa3eb04
James Clarendon <james.clarendon@gmail.com>
2024-08-26 13:43:30 -0700

    Example of event-only Structure Run polling.

--- example-client/client.py
+++ example-client/client.py
@@ -68,56 +68,72 @@ def run_structure(input: str) -> Optional[str]:
 
     received_event_ids = set()  # Keep track of which events we've already received.
     structure_has_terminal_event = False
-    
+
     # The client will continuously poll the Structure Run until it receives one of three events:
     # 1. Our Structure has voluntarily emitted a FinishStructureRunEvent
     # 2. The Structure has stopped running due to an error
     # 3. The Structure has stopped running without an error
     while not structure_has_terminal_event:
+        # TODO: Can the API be adjusted to give us all NEW events that occurred after a specific point?
         event_list = get_structure_run_events(
             host=HOST, api_key=GT_API_KEY, run_id=structure_run_id
         )
         events = event_list["events"]
-        
-        # Find all events that are new to us.      
+
+        # Find all events that are new to us.
         for event in events:
             event_id = event["event_id"]
             if event_id not in received_event_ids:
                 # Mark it as received.
                 received_event_ids.add(event_id)
-                
+
                 # Is this an event type that we care about?
                 event_type = event["value"]["type"]
-                
+
                 match event_type:
                     case "CompletionChunkEvent":
                         # If the agent is set to streaming, print out each streaming chunk
                         completion_token = event["value"]["token"]
                         print(completion_token, flush=True, end="")
                     case "FinishStructureRunEvent":
-                        
-                              
+                        # Our Griptape Structure told us that it was done, and gave us a payload for the output. Our client can stop polling the Structure.
+                        structure_has_terminal_event = True
+                        structure_output = event["value"]["output_task_output"]
+                        print(structure_output)
+                    case "SystemEvent.StructureRunExecutionFailed":
+                        # The system running our Griptape Structure reported that our code encountered an error. The client does not need to poll any further.
+                        structure_has_terminal_event = True
+                        errors = event["value"]["error messages"]
+                        raise ValueError(errors)
+                    case "SystemEvent.StructureRunExecutionSucceeded":
+                        # The system running our Griptape Structure reported that our code completed execution without erros. The client does not need to poll any further.
+                        structure_has_terminal_event = True
+                    case _:
+                        # All other event types land here.
+                        pass
 
         time.sleep(1)  # Poll every second.
 
-    logs = get_structure_run_logs(
-        host=HOST, api_key=GT_API_KEY, run_id=structure_run_id
-    )
 
-    if structure_run["status"] == "SUCCEEDED":
-        stdout = next(
-            (log["message"] for log in logs["logs"] if log["stream"] == "stdout"),
-            None,
-        )
-        print(stdout)
+# TODO: Today, logs take a long time to get, which kind of makes this code less useful. Could this info be put into the SystemEvents?
+#    logs = get_structure_run_logs(
+#        host=HOST, api_key=GT_API_KEY, run_id=structure_run_id
+#    )
 
-        return structure_run["output"]["value"] if "output" in structure_run else None
-    else:
-        stderr = next(
-            (log["message"] for log in logs["logs"] if log["stream"] == "stderr"),
-            None,
-        )
-        raise ValueError(stderr)
+#    if structure_run["status"] == "SUCCEEDED":
+#        stdout = next(
+#            (log["message"] for log in logs["logs"] if log["stream"] == "stdout"),
+#            None,
+#        )
+#        print(stdout)
+
+#        return structure_run["output"]["value"] if "output" in structure_run else None
+#    else:
+#        stderr = next(
+#            (log["message"] for log in logs["logs"] if log["stream"] == "stderr"),
+#            None,
+#        )
+#        raise ValueError(stderr)
 
 
 if __name__ == "__main__":
